8fbd26abf110a5414691c490724833c7edf32ee3
Alumasa <alumasa.v@gmail.com>
2022-09-04 19:17:29 +0000

    Original version

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 """Module for FileStorage class."""
+import datetime
 import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-
 
 
 class FileStorage:
@@ -20,11 +13,13 @@ class FileStorage:
 
     def all(self):
         """Returns __objects dictionary."""
+        # TODO: should this be a copy()?
         return FileStorage.__objects
 
     def new(self, obj):
         """Sets new obj in __objects dictionary."""
-        key = "{}.{}".format(type(obj).__class__.__name__, obj.id)
+        # TODO: should these be more precise specifiers?
+        key = "{}.{}".format(type(obj).__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
@@ -35,7 +30,15 @@ class FileStorage:
 
     def classes(self):
         """Returns a dictionary of valid classes and their references."""
-        classes = {"BaseModel": BaseModel,
+	from models.base_model import BaseModel
+	from models.user import User
+	from models.state import State
+	from models.city import City
+	from models.amenity import Amenity
+	from models.place import Place
+	from models.review import Review
+
+	classes = {"BaseModel": BaseModel,
                    "User": User,
                    "State": State,
                    "City": City,
@@ -50,5 +53,45 @@ class FileStorage:
             return
         with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dict = json.load(f)
-            FileStorage.__objects = {k: self.classes()[v["__class__"]](**v)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
+
+    def attributes(self):
+        """Returns the valid attributes and their types for classname."""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
